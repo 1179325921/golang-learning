ddd45b43df6dd9a792bc80da3cf7e0e5ed99b971
yuanshuai03 <yuanshuai03@qutoutiao.net>
2019-11-29 10:18:27 +0800

    feat(write file ): write file

---
+++ src/file/demo3/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	file, e := os.OpenFile("a.txt", os.O_CREATE|os.O_WRONLY, 0666)
+	if nil != e {
+		fmt.Println(e)
+	}
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+
+	str := "你好，golang\r\n"
+	for i := 0; i < 5; i++ {
+		_, e := writer.WriteString(str)
+		if e != nil {
+			fmt.Println(e)
+			return
+		}
+	}
+	e = writer.Flush()
+	if e != nil {
+		fmt.Println(e)
+	}
+	fmt.Println("结束")
+}
